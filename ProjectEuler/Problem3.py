782a6f8aead4f132450c27f4c125b09894fc146e
Tolsti <tolsti_web@mail.ru>
2019-12-06 23:06:07 +0300

    -

--- ProjectEuler/Problem3.py
+++ ProjectEuler/Problem3.py
@@ -1,20 +1,18 @@
 """Простые делители числа 13195 - это 5, 7, 13 и 29.
 Каков самый большой делитель числа 600851475143, являющийся простым числом?"""
 
-number = 600851475143
-flag = number - 1
-while flag > 1:
-    if number % flag == 0:
-        print(flag)
-    flag -= 1
-# Список простых чисел
-prime = [i for i in range(2, 10000)]
+prime = [i for i in range(2, 100000)]
 prime_list = list()
 prime_tmp = list()
 flag = 0
-while flag < 100:
+a = 0
+while flag < 1000:
     prime_list.append(prime[0])
-    prime_tmp = [i for i in prime if i % prime_list[flag] != 0]
+    a = prime[0]
+    prime_tmp = [i for i in prime if i % a != 0]
     prime = prime_tmp
     flag += 1
-print(prime_list)
+
+number = 600851475143
+arr = [i for i in range(2, 1000000) if number % i == 0 and i in prime_list]
+print(max(arr))
