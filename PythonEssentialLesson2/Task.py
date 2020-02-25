cc8c5f9d6087b6b5c004e16f8fbb1720167829ae
Tolsti <tolsti_web@mail.ru>
2020-02-25 22:27:06 +0300

    +

--- PythonEssentialLesson2/Task.py
+++ PythonEssentialLesson2/Task.py
@@ -1,3 +1,65 @@
 """Создайте иерархию классов транспортных средств. В общем классе опишите общие для всех транспортных средств поля,
 в наследниках – специфичные для них. Создайте несколько экземпляров.
 Выведите информацию о каждом транспортном средстве."""
+
+
+class Vehicle:
+    def __init__(self):
+        self.name = False
+        self.can_ride = False
+        self.can_swim = False
+        self.can_fly = False
+    
+    def show_functions(self):
+        print(str(self.name) + ':')
+        print('Ездит: ', self.can_ride)
+        print('Плавает: ', self.can_swim)
+        print('Летает: ', self.can_fly)
+        print()
+
+
+class Car(Vehicle):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Машина'
+        self.can_ride = True
+
+
+class Boat(Vehicle):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Лодка'
+        self.can_swim = True
+
+
+class Plane(Vehicle):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Самолет'
+        self.can_fly = True
+
+
+class Amphibious(Car, Boat):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Амфибия'
+
+
+class Agent_007s_car(Car, Boat, Plane):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Машина агента 007'
+
+
+if __name__ == '__main__':
+    car = Car()
+    boat = Boat()
+    plane = Plane()
+    amphibious = Amphibious()
+    agent_007s_car = Agent_007s_car()
+    
+    car.show_functions()
+    boat.show_functions()
+    plane.show_functions()
+    amphibious.show_functions()
+    agent_007s_car.show_functions()
--- PythonEssentialLesson2/Task1.py
+++ PythonEssentialLesson2/Task1.py
@@ -3,3 +3,35 @@
 Создайте подкласс ProEditor, в котором данный метод будет переопределён. Введите с клавиатуры лицензионный ключ и,
 если он корректный, создайте экземпляр класса ProEditor, иначе Editor.
 Вызовите методы просмотра и редактирования документов."""
+
+
+class Editor:
+    _reg_key = [1234, 4321, 'qwer']
+    
+    def __init__(self):
+        self.data_document = 'Я помню чудное мгновенье:\n'\
+                             'Передо мной явилась ты,\n'\
+                             'Как мимолетное виденье,\n'\
+                             'Как гений чистой красоты.\n'\
+                             '\n'\
+                             'В томленьях грусти безнадежной,\n'\
+                             'В тревогах шумной суеты,\n'\
+                             'Звучал мне долго голос нежный\n'\
+                             'И снились милые черты.'
+    
+    def view_document(self):
+        print(self.data_document)
+    
+    def edit_document(self):
+        print('Редактирование документа не доступно для бесплатной версии.')
+
+
+class ProEditor(Editor):
+    def edit_document(self):
+        super().edit_document()
+
+
+if __name__ == '__main__':
+    document = ProEditor()
+    # document.view_document()
+    document.edit_document()
