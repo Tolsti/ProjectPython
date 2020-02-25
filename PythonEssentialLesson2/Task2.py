1d72f560e662ae9da4b7430442373471012fe095
Tolsti <tolsti_web@mail.ru>
2020-02-26 01:34:14 +0300

    +

--- PythonEssentialLesson2/Task2.py
+++ PythonEssentialLesson2/Task2.py
@@ -1,2 +1,26 @@
 """Опишите классы графического объекта, прямоугольника и объекта, который может обрабатывать нажатия мыши.
 Опишите класс кнопки. Создайте объект кнопки и обычного прямоугольника. Вызовите метод нажатия на кнопку."""
+
+
+class Rectangle:
+    def __init__(self, side_a, side_b):
+        self.side_a = side_a
+        self.side_b = side_b
+
+
+class ObjectSwitch:
+    def click_button(self):
+        if not Button.switch:
+            Button.switch = True
+        else:
+            Button.switch = False
+
+
+class Button(Rectangle):
+    switch = False
+
+
+if __name__ == '__main__':
+    button = Button(4, 8)
+    rectangle = Rectangle(3, 7)
+    obj = ObjectSwitch()
--- PythonEssentialLesson2/Task3.py
+++ PythonEssentialLesson2/Task3.py
@@ -1,3 +1,48 @@
 """Создайте иерархию классов с использованием множественного наследования.
 Выведите на экран порядок разрешения методов для каждого из классов. Объясните,
 почему линеаризации данных классов выглядят именно так."""
+
+
+class A:
+    pass
+
+
+class B(A):
+    pass
+
+
+class C(A):
+    pass
+
+
+class D(B, C):
+    pass
+
+
+class E:
+    pass
+
+
+class F(D, E):
+    pass
+
+
+if __name__ == '__main__':
+    print('Order of allowed methods of class A:')
+    for i in A.mro():
+        print(i)
+    print('\nOrder of allowed methods of class B:')
+    for i in B.mro():
+        print(i)
+    print('\nOrder of allowed methods of class C:')
+    for i in C.mro():
+        print(i)
+    print('\nOrder of allowed methods of class D:')
+    for i in D.mro():
+        print(i)
+    print('\nOrder of allowed methods of class E:')
+    for i in E.mro():
+        print(i)
+    print('\nOrder of allowed methods of class F:')
+    for i in F.mro():
+        print(i)
