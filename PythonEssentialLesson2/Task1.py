2b735782b767662c406c50f0068ce78751bf1f1d
Tolsti <tolsti_web@mail.ru>
2020-02-25 23:26:52 +0300

    +

--- PythonEssentialLesson2/Task1.py
+++ PythonEssentialLesson2/Task1.py
@@ -6,32 +6,44 @@
 
 
 class Editor:
-    _reg_key = [1234, 4321, 'qwer']
-    
-    def __init__(self):
-        self.data_document = 'Я помню чудное мгновенье:\n'\
-                             'Передо мной явилась ты,\n'\
-                             'Как мимолетное виденье,\n'\
-                             'Как гений чистой красоты.\n'\
-                             '\n'\
-                             'В томленьях грусти безнадежной,\n'\
-                             'В тревогах шумной суеты,\n'\
-                             'Звучал мне долго голос нежный\n'\
-                             'И снились милые черты.'
-    
+    _reg_key = ['1234', '4321', 'qwerty']
+
+    def __init__(self, data_document):
+        self.data_document = data_document
+
     def view_document(self):
         print(self.data_document)
-    
+
     def edit_document(self):
         print('Редактирование документа не доступно для бесплатной версии.')
 
 
 class ProEditor(Editor):
     def edit_document(self):
-        super().edit_document()
+        # super().edit_document()
+        print('Редактирование документа.')
+        self.data_document = input('Введите данные нового документа: ')
 
 
 if __name__ == '__main__':
-    document = ProEditor()
-    # document.view_document()
+    doc = 'Я помню чудное мгновенье:\n' \
+          'Передо мной явилась ты,\n' \
+          'Как мимолетное виденье,\n' \
+          'Как гений чистой красоты.\n' \
+          '\n' \
+          'В томленьях грусти безнадежной,\n' \
+          'В тревогах шумной суеты,\n' \
+          'Звучал мне долго голос нежный\n' \
+          'И снились милые черты.'
+    if input('Введите ключ регистрации: ') in Editor._reg_key:
+        print('У Вас платная версия.')
+        document = ProEditor(doc)
+    else:
+        print('У Вас бесплатная версия.')
+        document = Editor(doc)
+    document.view_document()
+    print('---------------------------')
     document.edit_document()
+    print('---------------------------')
+
+    document.view_document()
