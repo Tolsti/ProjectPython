234f0adf553739a45f5b2a540ce26d0cd8bf73aa
Tolsti <tolsti_web@mail.ru>
2020-04-21 16:01:34 +0300

    +

--- PythonEssentialLesson5/Task4.py
+++ PythonEssentialLesson5/Task4.py
@@ -1,1 +1,30 @@
 """Ознакомьтесь при помощи документации с классами namedtuple и deque модуля collections."""
+
+import collections
+import string
+
+parts = collections.namedtuple('Parts', 'id_num desk cost amount')
+
+auto_parts = parts(id_num='4352',
+                   desk='Skoda Superb',
+                   cost=8900,
+                   amount=1)
+print(*auto_parts)
+for part in auto_parts:
+    print(part)
+print(auto_parts.id_num)
+
+#######################################################################################################################
+
+d = collections.deque(string.ascii_lowercase)
+print(*d)
+
+d.append('bork')
+print(*d)
+
+d.appendleft('test')
+print(*d)
+
+d.rotate(5)
+print(*d)
+
